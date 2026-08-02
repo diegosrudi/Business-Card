8e29f9b1de9bc264bba79199ade85ec47384e945
Diego Rocha <diegosr.udi@gmail.com>
2026-08-02 13:07:37 -0300

    correction

--- components/Info.jsx
+++ components/Info.jsx
@@ -3,7 +3,7 @@ export default function Info() {
         <header>
             <img className="bio-photo" src="./images/eu.jpg"></img>
             <h1>Diego Rocha</h1>
-            <h3>FullStake Developer</h3>
+            <h3>FullStack Developer</h3>
             <p>diegorocha.website</p>
             <div className="botoes">
                 <a className="email" href="mailto:diegosr.udi@gmail.com"><i class="fa-solid fa-envelope"></i>Email</a>
